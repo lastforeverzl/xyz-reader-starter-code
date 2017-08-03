376dbcc9a84c61f48925231ab6ffc177aa229961
lastforeverzl <lastforeverzl@gmail.com>
2017-08-02 18:00:34 -0700

    fab hide show

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/MyFabBehavior.java
@@ -0,0 +1,54 @@
+package com.example.xyzreader.ui;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by lei on 8/2/17.
+ */
+
+public class MyFabBehavior extends FloatingActionButton.Behavior {
+
+    public MyFabBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public boolean onStartNestedScroll(final CoordinatorLayout coordinatorLayout,
+                                       final FloatingActionButton child,
+                                       final View directTargetChild, final View target,
+                                       final int nestedScrollAxes) {
+        // Ensure we react to vertical scrolling
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL
+                || super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, nestedScrollAxes);
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child,
+                               View target, int dxConsumed, int dyConsumed, int dxUnconsumed,
+                               int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed,
+                dyUnconsumed);
+
+        Log.d("MyFabBehavior", "dyConsumed: " + dyConsumed);
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.hide();
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            for (int i = 0; i < coordinatorLayout.getChildCount(); i++) {
+                if (coordinatorLayout.getChildAt(i) instanceof Snackbar.SnackbarLayout) {
+                    child.show();
+                    return;
+                }
+            }
+
+            child.setTranslationY(0.0f);
+            child.show();
+        }
+    }
+}
